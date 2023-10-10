3aaed59e0a44feef3f88f4b2a793860d023d4c5c
jar <dev.bazylev@gmail.com>
2023-10-10 19:27:14 +0300

    add functions

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function checkString1820(x, y) {
+  if (x.length < y) {
+    return x.length;
+  }
+}
+
+checkString1820('fgd', 20);
+
+function checkString10(x, y) {
+  if (x.length > y) {
+    return x.length;
+  }
+}
+
+checkString10('sdfg', 10);
