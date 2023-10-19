a28e5f0308f805ae4b2471a103604ad6cfa1deba
jar <dev.bazylev@gmail.com>
2023-10-19 12:22:23 +0300

    add 1

---
+++ js/functions.js
@@ -0,0 +1,70 @@
+const checkString = (stringChars, lengthMax) =>
+  stringChars.length <= lengthMax;
+
+checkString('проверяемая строка', 20);
+checkString('проверяемая строка', 18);
+checkString('проверяемая строка', 10);
+
+function checkPalindrom(rawString) {
+  const string = rawString.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.at(i) !== string.at(-i - 1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+checkPalindrom('топот');
+checkPalindrom('ДовОд');
+checkPalindrom('Кекс');
+checkPalindrom('Лёша на полке клопа нашёл ');
+
+function extractNumber(arg) {
+  const string = arg.toString();
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
+    }
+  }
+  return parseInt(result,10);
+}
+
+extractNumber('2023 год');
+extractNumber('ECMAScript 2022');
+extractNumber('1 кефир, 0.5 батона');
+extractNumber('агент 007');
+extractNumber('а я томат');
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function getRandomPositiveInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
