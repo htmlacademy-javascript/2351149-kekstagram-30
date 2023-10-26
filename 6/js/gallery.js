3ffdefd6907a36d8906d65644a5e0bf98a3c8fcb
Keks <keks@htmlacademy.ru>
2023-10-26 10:54:47 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/gallery.js
+++ 6/js/gallery.js
@@ -9,7 +9,7 @@ generatedPictures.forEach(({url, description, likes, comments}) => {
   const clonedTemplate = templatePictures.cloneNode(true);
   clonedTemplate.querySelector('.picture__img').src = url;
   clonedTemplate.querySelector('.picture__img').alt = description;
-  clonedTemplate.querySelector('.picture__comments').textContent = comments;
+  clonedTemplate.querySelector('.picture__comments').textContent = comments.length;
   clonedTemplate.querySelector('.picture__likes').textContent = likes;
   fragmentPictures.appendChild(clonedTemplate);
 });
