23e5e13c758ae10a273fa379d8617a67eab40290
Keks <keks@htmlacademy.ru>
2023-11-24 14:09:55 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/form.js
@@ -0,0 +1,156 @@
+import { isKeydown } from './util.js';
+import { resetScale } from './scale.js';
+import { initEffect, resetEffect } from './effect.js';
+import { sendPictures } from './api.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
+
+const MAX_HASHTAG_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const FILE_TYPES = ['.img', '.png', '.jpg'];
+const ErrorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хэштегов`,
+  NOT_UNIQUE: 'Хэштеги должны быть уникальными',
+  INVALID_PATTERN: 'Неправильный хэштег',
+};
+
+const submitButtonCaption = {
+  SUBMITTING: 'Отправляю...',
+  IDLE: 'Опубликовать',
+};
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = form.querySelector('.img-upload__overlay');
+const cancelButton = form.querySelector('.img-upload__cancel');
+const fileField = form.querySelector('.img-upload__input');
+const hashtagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text__description');
+const submitButton = form.querySelector('.img-upload__submit');
+const thumbnails = form.querySelectorAll('.effects__preview');
+const preview = form.querySelector('.img-upload__preview img');
+
+const toggleSubmitButton = (isDisabled) => {
+  if (isDisabled) {
+    submitButton.textContent = submitButtonCaption.SUBMITTING;
+  } else {
+    submitButton.textContent = submitButtonCaption.IDLE;
+  }
+};
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  resetScale();
+  resetEffect();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+const normalizeTags = (tagString) => tagString
+  .trim()
+  .split()
+  .filter((tag) => Boolean(tag.lentgh));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const isErrorMessageExists = () => Boolean(document.querySelector('.error'));
+
+function onDocumentKeydown(evt) {
+  if (isKeydown(evt, 'Escape') && !isTextFieldFocused() && !isErrorMessageExists()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const sendForm = async (formElement) => {
+  if (!pristine.validate()) {
+    return;
+  }
+
+  try {
+    toggleSubmitButton(true);
+    await sendPictures(new FormData(formElement));
+    toggleSubmitButton(false);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+    toggleSubmitButton(false);
+  }
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  sendForm(evt.target);
+};
+
+pristine.addValidator(
+  hashtagField,
+  hasValidCount,
+  ErrorText.INVALID_COUNT,
+  3,
+  true
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  ErrorText.NOT_UNIQUE,
+  2,
+  true
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidTags,
+  ErrorText.INVALID_PATTERN,
+  1,
+  true
+);
+
+const onUploadImage = () => {
+  const file = fileField.files[0];
+  const fileModified = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((item) => fileModified.endsWith(item));
+  if (matches) {
+    preview.src = URL.createObjectURL(file);
+  }
+  thumbnails.forEach((item) => {
+    item.style.backgroundImage = `url("${URL.createObjectURL(file)}")`;
+  });
+  showModal();
+};
+
+fileField.addEventListener('change', onUploadImage);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
+
+initEffect();
